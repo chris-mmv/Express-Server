6dfeb717917042c9cc56dc4f86b8d035997572e9
chris-mmv <chris.mmv@gmail.com>
2025-12-23 16:43:44 -0500

    Working

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const topSpots = require('./data.json');
+
+
+// const port = 3000; in index.js
+
 
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running");
+});
+
+app.get("/data", (req, res) => {
+  res.status(200).json(topSpots);
+});
 
 // finally export the express application
 module.exports = app;
