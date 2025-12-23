b0008d5937d6a8d61b58e0461e8b24f4fb3b0290
chris-mmv <chris.mmv@gmail.com>
2025-12-23 16:46:33 -0500

    const port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 // here we're importing your express app
 const server = require('./app');
+const port = 3000;
 
 // We're telling the app to listen for network requests on port 3000
-server.listen(3000, function() {
-  console.log('Server is listening on http://localhost:3000');
+server.listen(port, function() {
+  console.log('Server is listening on http://localhost:' + port);
 });
